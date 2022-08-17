0a3fd3dcf0d605dc57b5db20ac38672e91af9a0c
Mike M <16300181+sometastycake@users.noreply.github.com>
2022-08-17 22:07:37 +0300

    Get profile info

--- core/steam/api/account.py
+++ core/steam/api/account.py
@@ -1,7 +1,9 @@
+import json
 import re
 from typing import Optional
 
 from aiohttp import FormData
+from lxml.html import HtmlElement, document_fromstring
 from steam.api.enums import Language
 from steam.api.errors import NotFoundSteamid
 from steam.api.schemas import ProfileInfo, ProfileInfoResponse
@@ -12,7 +14,13 @@ class SteamAccountAPI:
 
     def __init__(self, steam: Steam, steamid: Optional[int] = None):
         self.steam = steam
-        self.steamid = steamid
+        self._steamid = steamid
+
+    @property
+    async def steamid(self) -> int:
+        if not self._steamid:
+            self._steamid = await self.get_steamid()
+        return self._steamid
 
     async def get_steamid(self) -> int:
         response = await self.steam.request(
@@ -47,6 +55,27 @@ class SteamAccountAPI:
         )
         return True if response == 'true' else False
 
+    async def get_profile_info(self) -> ProfileInfo:
+        response = await self.steam.request(
+            url=f'https://steamcommunity.com/profiles/{await self.steamid}/edit/info',
+            headers={
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                          'image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            },
+        )
+        page: HtmlElement = document_fromstring(response)
+        info = json.loads(page.cssselect('#profile_edit_config')[0].attrib['data-profile-edit'])
+        return ProfileInfo(
+            personaName=info['strPersonaName'],
+            real_name=info['strRealName'],
+            customURL=info['strCustomURL'],
+            summary=info['strSummary'],
+            country=info['LocationData']['locCountry'],
+            state=info['LocationData']['locState'],
+            city=info['LocationData']['locCity'],
+            hide_profile_awards=info['ProfilePreferences']['hide_profile_awards'],
+        )
+
     async def set_profile_info(self, info: ProfileInfo) -> None:
         data = FormData(
             fields=[
@@ -58,17 +87,14 @@ class SteamAccountAPI:
                 ('json', '1'),
             ],
         )
-        steamid = self.steamid
-        if not steamid:
-            steamid = await self.get_steamid()
         response: ProfileInfoResponse = await self.steam.request(
             method='POST',
-            url=f'https://steamcommunity.com/profiles/{steamid}/edit/',
+            url=f'https://steamcommunity.com/profiles/{await self.steamid}/edit/',
             data=data,
             headers={
                 'Accept': 'application/json, text/plain, */*',
                 'Origin': 'https://steamcommunity.com',
-                'Referer': f'https://steamcommunity.com/profiles/{steamid}/edit/info',
+                'Referer': f'https://steamcommunity.com/profiles/{await self.steamid}/edit/info',
             },
             response_model=ProfileInfoResponse,
         )
