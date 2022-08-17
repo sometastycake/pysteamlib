39afca8473c7690d2866fd96cc6bb5dc01596ce5
Mike M <16300181+sometastycake@users.noreply.github.com>
2022-08-17 04:17:41 +0300

    Getting steamid

--- core/steam/api/account.py
+++ core/steam/api/account.py
@@ -1,4 +1,7 @@
+import re
+
 from steam.api.enums import Language
+from steam.api.errors import NotFoundSteamid
 from steam.steam import Steam
 
 
@@ -6,6 +9,22 @@ class SteamAccountAPI:
 
     def __init__(self, steam: Steam):
         self.steam = steam
+
+    async def get_steamid(self) -> int:
+        response = await self.steam.request(
+            url='https://steamcommunity.com/',
+            headers={
+                'Accept': '*/*',
+                'Origin': 'https://steamcommunity.com',
+            }
+        )
+        steamid = re.search(
+            pattern='g_steamID = \"(\d+)\";',
+            string=response,
+        )
+        if not steamid:
+            raise NotFoundSteamid
+        return int(steamid.group(1))
 
     async def change_account_language(self, language: Language) -> bool:
         response = await self.steam.request(
