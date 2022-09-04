915d500ae1cb08084e51d94e15ed9a65086b5854
Mike M <16300181+sometastycake@users.noreply.github.com>
2022-09-04 19:05:35 +0300

    Fixed SteamError

--- steam/exceptions.py
+++ steam/exceptions.py
@@ -11,7 +11,7 @@ class SteamError(Exception):
 
     def __str__(self) -> str:
         return str({
-            'error': STEAM_ERROR_CODES[self.error_code],
+            'error': STEAM_ERROR_CODES[self.error_code] if self.error_code in STEAM_ERROR_CODES else self.error_code,
             'msg': self.error_msg,
         })
 
