1e1a7d3100a7ed7096f1bfd8864c59c093a652fe
Mike M <16300181+sometastycake@users.noreply.github.com>
2022-09-18 22:49:21 +0300

    Docstrings in exceptions

--- steamlib/api/trade/exceptions.py
+++ steamlib/api/trade/exceptions.py
@@ -1,42 +1,42 @@
 class SendOfferError(Exception):
-    ...
+    """Error sending exchange."""
 
 
 class SteamNullResponseError(SendOfferError):
-    ...
+    """If response from Steam is null."""
 
 
 class SteamServerDownError(SendOfferError):
-    ...
+    """Steam servers may be down."""
 
 
 class TradeOffersLimitError(SendOfferError):
-    ...
+    """Trade offers limit."""
 
 
 class AccountOverflowError(SendOfferError):
-    ...
+    """Account overflow."""
 
 
 class TradeBanError(SendOfferError):
-    ...
+    """Account have a trade ban."""
 
 
 class ProfileSettingsError(SendOfferError):
-    ...
+    """Incorrect profile settings."""
 
 
 class TradelinkError(SendOfferError):
-    ...
+    """Tradelink may be incorrect."""
 
 
 class NotFoundMobileConfirmationError(Exception):
-    ...
+    """No offer found pending mobile confirmation."""
 
 
 class InvalidAuthenticatorError(Exception):
-    ...
+    """Invalid authenticator."""
 
 
 class InvalidConfirmationPageError(Exception):
-    ...
+    """Invalid confirmation page."""
